6d2b18139b20d4129c3b4e39bac47b4b94b90615
Juicebox Jones <jderhalli@gmail.com>
2019-06-24 18:15:24 -0700

    drink detail page displaying properly. Consulting API for extended features.

--- src/Components/Drink.js
+++ src/Components/Drink.js
@@ -2,96 +2,99 @@ import React, { Component } from 'react';
 import ApiService from '../ApiServices/Services';
 
 class Drink extends Component {
-  state = { 
-    drink:''
-   }
+  state = {
+    drink: ''
+  };
 
- componentDidMount(){
+  componentDidMount() {
     let drinkId = this.props.match.params.drinkId;
-    ApiService.getDrinkDetails(drinkId)
-    .then(data => 
+    ApiService.getDrinkDetails(drinkId).then(data =>
       this.setState({
-        drink:(data.drinks[0])
+        drink: data.drinks[0]
       })
-      )
+    );
+  }
+
+  handleBack = () => {
+    this.props.history.goBack()
   }
 
   drinkData = () => {
-    let drinkInfo = this.state.drink
-    console.log(drinkInfo)
+    let drinkInfo = this.state.drink;
+    console.log(drinkInfo);
     return (
       <>
-      <div className='heading-ctr'>
-      <h3 className='drink-name'>
-        {drinkInfo.strDrink}
-      </h3>
-      <p className='drink-type'>
-        Type: {drinkInfo.strAlcoholic} / {drinkInfo.strCategory}
-      </p>
-      <img src={drinkInfo.strDrinkThumb} alt={drinkInfo.strDrink}/>
-      </div>
-      <p>Serve in: {drinkInfo.strGlass}</p>
-      <p>{this.ingredientData(drinkInfo)}</p>
+        <div className="heading-ctr">
+          <h3 className="drink-name">{drinkInfo.strDrink}</h3>
+          <p className="drink-type">
+            Type: {drinkInfo.strAlcoholic} / {drinkInfo.strCategory}
+          </p>
+          <img src={drinkInfo.strDrinkThumb} alt={drinkInfo.strDrink} />
+        </div>
+        <p>Serve in: {drinkInfo.strGlass}</p>
+        <p />
+        <h4>Ingredients/Measurements:</h4>
+        <p className="ingList">
+          {drinkInfo.strMeasure1} {drinkInfo.strIngredient1}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure2} {drinkInfo.strIngredient2}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure3} {drinkInfo.strIngredient3}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure4} {drinkInfo.strIngredient4}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure5} {drinkInfo.strIngredient5}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure6} {drinkInfo.strIngredient6}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure7} {drinkInfo.strIngredient7}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure8} {drinkInfo.strIngredient8}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure9} {drinkInfo.strIngredient9}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure10} {drinkInfo.strIngredient10}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure11} {drinkInfo.strIngredient11}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure12} {drinkInfo.strIngredient12}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure13} {drinkInfo.strIngredient13}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure14} {drinkInfo.strIngredient14}
+        </p>
+        <p className="ingList">
+          {drinkInfo.strMeasure15} {drinkInfo.strIngredient15}
+        </p>
+        <p />
+        <h4>Directions:</h4>
+        <p>{drinkInfo.strInstructions}</p>
+        <button type='button' onClick={e => this.handleBack(e)}>Back</button>
       </>
-      //work on displaying ingredients next
-    )
-  }
+    );
+  };
 
-//   strIBA: null
-// strIngredient1: "Dark rum"
-// strIngredient2: "Light rum"
-// strIngredient3: "Vodka"
-// strIngredient4: "Orange juice"
-// strIngredient5: ""
-// strIngredient6: ""
-// strIngredient7: ""
-// strIngredient8: ""
-// strIngredient9: ""
-// strIngredient10: ""
-// strIngredient11: ""
-// strIngredient12: ""
-// strIngredient13: ""
-// strIngredient14: ""
-// strIngredient15: ""
-// strInstructions: "Blend with ice. Serve in a wine glass. Garnish with carrot."
-// strInstructionsDE: null
-// strInstructionsES: null
-// strInstructionsFR: null
-// strInstructionsZH-HANS: null
-// strInstructionsZH-HANT: null
-// strMeasure1: "1 shot "
-// strMeasure2: "2 shots "
-// strMeasure3: "1 shot "
-// strMeasure4: "1 shot "
-// strMeasure5: " "
-// strMeasure6: " "
-// strMeasure7: " "
-// strMeasure8: " "
-// strMeasure9: " "
-// strMeasure10: ""
-// strMeasure11: ""
-// strMeasure12: ""
-// strMeasure13: ""
-// strMeasure14: ""
-// strMeasure15: ""
-// strTags: null
-// strVideo: null
-
-  ingredientData = drink =>{
-    for(let i = 1; i<16; i++){
-      console.log(i)
-      }
-  }
-
-
-  render() { 
-    console.log(this.props.match.params.drinkId)
-    return ( 
-      <div className=''>
-        {!this.state.drink ? console.log('no drink', this.state.drink) : this.drinkData()}
+  render() {
+    console.log(this.props.match.params.drinkId);
+    return (
+      <div className="">
+        {!this.state.drink ? 'Loading...' : this.drinkData()}
       </div>
-     );
+    );
   }
 }
- 
+
 export default Drink;
